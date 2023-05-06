44e194e91cd34b30f17355ab293802a3550045d1
sangdee <tkdwl06@gmail.com>
2023-05-06 21:25:32 +0900

    add initDirectFieldAccess

---
+++ src/main/java/com/example/template/base/advice/WebControllerAdvice.java
@@ -0,0 +1,14 @@
+package com.example.template.base.advice;
+
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.InitBinder;
+
+@ControllerAdvice
+public class WebControllerAdvice {
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.initDirectFieldAccess();
+    }
+}
